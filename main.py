abd2e0759f3ad3605e8387f1412e50789ce66e6f
Leonardo Florentino <leonardo.florentino@omie.com.br>
2022-06-30 14:46:57 -0300

    Updates

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from src.game import Game
-from pprint import pprint
-import statistics
 from src.report import Report
 
 MAX_GAME_SIMULATION = 300
@@ -12,6 +10,10 @@ if __name__ == "__main__":
     while i <= MAX_GAME_SIMULATION:
         simulations.append(Game().play())
         i += 1
-    pprint(f"The mean of duration of lap is: {Report.get_mean_lap(simulations)}")
-    pprint(f"The number of games that reached max lap is: {Report.get_reached_max_lap(simulations)}")
-    pprint(f"The profile of player reccurent is: {Report.get_profile_player_reccurent(simulations)}")
+    print(f"The mean of duration of lap is: {Report.get_mean_lap(simulations)}")
+    print(
+        f"The number of games that reached max lap is: {Report.get_reached_max_lap(simulations)}"
+    )
+    print(
+        f"The profile of player reccurent / Reccurent percent profile player: {Report.get_profile_player_reccurent(simulations)}"
+    )
